761f4960012ec634faf20307a9c1aa0498391b52
bhakti <bhaktishinde2388@gmail.com>
2024-08-16 17:59:06 +0530

    on farmer dashboard the logged farmer named is displayed.and creted the container for rnndering on adding the vegetable page

--- client/src/views/FarmerDashboard/FarmerDashboard.js
+++ client/src/views/FarmerDashboard/FarmerDashboard.js
@@ -1,8 +1,63 @@
 import React from 'react'
+import "./FarmerDashboard.css"
+import { useEffect,useState} from 'react'
+import toast, {Toaster} from 'react-hot-toast'
+import Add from "./add (1).png"
+import {Link} from "react-router-dom"
 
 function FarmerDashboard() {
+    const [user,setUser]=useState('')
+
+
+    //farmer
+
+    useEffect(() => {
+        const currentUser = JSON.parse(localStorage.getItem('currentUser'))
+        if(currentUser){
+          setUser(currentUser)
+        }
+        
+        
+        if(!currentUser){
+          window.location.href='/login'
+        }
+        },[])
+        
   return (
-    <div>FarmerDashboard</div>
+    <div>
+         <h1 className='user-greeting'>Hello.... <span className='user-greeting-name'>{user.fullName}😊</span></h1>
+      <h3 className='heading'>अब हर किसान बढेगा!!👩‍🌾👨‍🌾
+        <br>
+        </br>
+        अपने श्रम का अच्छा मोल पाऐगा!!
+      </h3>
+
+
+{/* logout */}
+      <span className='logout'
+      onClick={()=>{
+        localStorage.clear()
+        toast.success("logout successfully")
+        setTimeout(()=>{
+          window.location.href = '/login'
+        }, 2000)
+      }}
+      
+      >Logout</span>
+
+      <div className='add-img-link-container d-flex flex-wrap justify-content-evenly '>
+        <h1 className='about'>
+          click here for adding your vegetables👇
+        </h1>
+        
+     
+        <Link to="/addTransaction">
+      <img src={Add} className='add-img'/>
+      </Link>
+      </div>
+
+      <Toaster/>
+    </div>
   )
 }
 
